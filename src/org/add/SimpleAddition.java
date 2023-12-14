5ee62f93da621be4487a8841dfc67b03d1b2620e
Ramkumar090986 <ramkumar8687@gmail.com>
2023-12-14 11:04:26 +0530

    Addition code

---
+++ src/org/add/SimpleAddition.java
@@ -0,0 +1,15 @@
+package org.add;
+
+public class SimpleAddition {
+	
+	public static void main(String[] args) {
+		
+		int a=10,b=5,sum=0;
+		
+		sum=a+b;
+		
+		System.out.println("Addition of two numbers:"+sum);
+		
+	}
+
+}
